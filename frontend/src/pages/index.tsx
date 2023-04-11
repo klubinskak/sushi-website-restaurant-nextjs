67bb4a9066dbb9708b3ae20ee04b2e0d28559945
Klaudia Klubińska <klubinskaklaudia@gmail.com>
2023-04-11 15:46:12 +0200

    Revert "backround-imafe-fixes"
    This reverts commit 061e18fb7fdb8206388b205277ace3f43b991751.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -17,9 +17,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-full h-[100vh] ">
+      <div className="w-full ">
         <div className="bg-black">
-          <div className="bg-[url('/background.jpg')] bg-contain h-screen bg-no-repeat">
+          <div className="bg-[url('/background.jpg')] bg-cover bg-no-repeat">
             <Navbar number={80}/>
             <Hero />
             <Carousel />
