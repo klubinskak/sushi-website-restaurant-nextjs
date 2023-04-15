6a6069d4bbda5eacfed615050ea4d6c50e0369f3
Klaudia Klubińska <klubinskaklaudia@gmail.com>
2023-04-15 09:58:57 +0200

    env settings

--- frontend/components/Image.tsx
+++ frontend/components/Image.tsx
@@ -9,14 +9,11 @@ function OptimizedImage({image_url, name}: Props) {
   return (
     <>
       <Image
-      priority
         src={image_url}
         alt={name}
         width={600}
         height={400}
         className="w-[600px] h-[400px]"
-        blurDataURL="URL"
-        placeholder="blur"
       />
     </>
   )
--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     dataset: 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "3m4mx93m" ,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: '2023-01-01',
     useCdn: 'production',
 }
